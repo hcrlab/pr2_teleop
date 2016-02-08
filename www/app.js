ad8ce9f7d729d12023c81599a0bc58eb55c7dd74
Justin Huang <jstn@cs.washington.edu>
2016-02-08 14:42:53 -0800

    Update websocket URL.

--- www/app.js
+++ www/app.js
@@ -40,44 +40,50 @@ var Pr2TeleopApp = (function() {
 
   // Initializes the teleop app.
   function init(base_controller, head_controller, tuck_arms_controller, touch_interface) {
-    $.get('/get_websocket_url', function(websocket_url) {
-      _ros = new ROSLIB.Ros({
-        url: websocket_url
-      });
-      _ros.on('connection', function() {
-        console.log('Connected to websocket server.');
-      });
-      _ros.on('error', function(error) {
-        console.log('Error connecting to websocket server: ', error);
-      });
-      _ros.on('close', function() {
-        console.log('Connection to websocket server closed.');
-      });
+    var websocketUrl = (function() {
+      var hostname = window.location.hostname;
+      var protocol = 'ws:';
+    
+        protocol = 'wss:'
+      }
+      return protocol + '//' + hostname + ':9090';
+    })();
+    _ros = new ROSLIB.Ros({
+      url: websocketUrl
+    });
+    _ros.on('connection', function() {
+      console.log('Connected to websocket server.');
+    });
+    _ros.on('error', function(error) {
+      console.log('Error connecting to websocket server: ', error);
+    });
+    _ros.on('close', function() {
+      console.log('Connection to websocket server closed.');
+    });
 
-      _waitForHeadImage();
-      _updateHeadImage();
-      $(window).resize(_updateHeadImage);
-      
-      base_controller.init(_ros, '/base_controller/command');
-      head_controller.init(_ros, '/head_traj_controller/point_head_action');
-      head_controller.sendGoal(0, 0);
-      tuck_arms_controller.init(_ros, '/tuck_arms');
-      touch_interface.init();
+    _waitForHeadImage();
+    _updateHeadImage();
+    $(window).resize(_updateHeadImage);
+    
+    base_controller.init(_ros, '/base_controller/command');
+    head_controller.init(_ros, '/head_traj_controller/point_head_action');
+    head_controller.sendGoal(0, 0);
+    tuck_arms_controller.init(_ros, '/tuck_arms');
+    touch_interface.init();
 
-      touch_interface.setMoveForward(base_controller.moveForward);
-      touch_interface.setMoveBackward(base_controller.moveBackward);
-      touch_interface.setMoveLeft(base_controller.moveLeft);
-      touch_interface.setMoveRight(base_controller.moveRight);
-      touch_interface.setRotateClockwise(base_controller.rotateClockwise);
-      touch_interface.setRotateCounterClockwise(base_controller.rotateCounterClockwise);
-      touch_interface.setHeadUp(head_controller.lookUp);
-      touch_interface.setHeadDown(head_controller.lookDown);
-      touch_interface.setHeadLeft(head_controller.lookLeft);
-      touch_interface.setHeadRight(head_controller.lookRight);
+    touch_interface.setMoveForward(base_controller.moveForward);
+    touch_interface.setMoveBackward(base_controller.moveBackward);
+    touch_interface.setMoveLeft(base_controller.moveLeft);
+    touch_interface.setMoveRight(base_controller.moveRight);
+    touch_interface.setRotateClockwise(base_controller.rotateClockwise);
+    touch_interface.setRotateCounterClockwise(base_controller.rotateCounterClockwise);
+    touch_interface.setHeadUp(head_controller.lookUp);
+    touch_interface.setHeadDown(head_controller.lookDown);
+    touch_interface.setHeadLeft(head_controller.lookLeft);
+    touch_interface.setHeadRight(head_controller.lookRight);
 
-      touch_interface.setTuckArms(function() {
-        tuck_arms_controller.tuckArms(true, true);
-      });
+    touch_interface.setTuckArms(function() {
+      tuck_arms_controller.tuckArms(true, true);
     });
   }
 
